2539b567e36e2a9aecdc5e3789b5c7eb1cd86639
jack <jack90john@hotmail.com>
2019-06-19 10:45:47 +0800

    Initial commit

---
+++ demo-eureka/src/main/java/com/demo/demo/eureka/DemoEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaApplication.class, args);
+    }
+
+}
